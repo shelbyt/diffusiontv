3b2e4b98879d560303779250f9c39e58b1cf9928
Shelby Thomas <shelbyt@ucsd.edu>
2023-11-15 15:49:53 -0800

    feed algorithm api for selection

--- src/pages/api/videos/index.ts
+++ src/pages/api/videos/index.ts
@@ -1,117 +1,130 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import prisma from '../../../utils/prismaClient'
-import { NSFWLevel } from '@prisma/client';
+//@ts-nocheck
+import { PrismaClient } from '@prisma/client';
 
-
-// Define the base URLs for your S3 bucket for videos and thumbnails
+const prisma = new PrismaClient();
 const VIDEO_BASE_URL = 'https://civ-all-encoded.media-storage.us-west.qencode.com/';
 //const THUMBS_BASE_URL = 'https://thumbs-all.media-storage.us-west.qencode.com/';
 //const THUMBS_BASE_URL = 'https://ps-lofiagihab.s3.us-west-2.amazonaws.com/';
 const THUMBS_BASE_URL = 'https://d10bxkdso1dzcx.cloudfront.net/';
 
 
-export interface IDbData {
-    videoId: string;
-    remoteId: number;
-    likeCount: number | null;
-    heartCount: number | null;
-    commentCount: number | null;
-    username: string | null;
-    user: {
-        imageUrl: string | null;
-    } | null;
-}
+// Function to fetch a random item from a category
+async function getRandomItemFromCategory(categoryId) {
+    const totalItems = await prisma.Image.count({
+        where: {
+            category: categoryId,
+            likeCount: {
+                gt: 15 // Only select items with more than 10 likes
+            }
+        },
 
-export interface IDataStorage {
-    videoUrl: string;
-    thumbUrl: string;
-}
+    });
 
-export interface IVideoData {
-    data: {
-        dbData: IDbData;
-        storage: IDataStorage;
-    }
-}
-const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    const pageSize = 5; // Results per page
-    let currentPage = 1;
-    if (req.query.currentPage) {
-        currentPage = Number(req.query.currentPage);
-    }
-
-    const oneWeekAgo = new Date();
-    oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
-
-    const twoWeeksAgo = new Date();
-    twoWeeksAgo.setDate(twoWeeksAgo.getDate() - 2);
-
-
-    const { method } = req.query;
-
-    async function getTopImages() {
-        const images = await prisma.image.findMany({
-            skip: (currentPage - 1) * pageSize,
-            take: pageSize,
-            // orderBy: {
-            //     createdAt: 'desc',
-            // },
-            // where: {
-            //     likeCount: {
-            //         gt: 5
-            //     },
-            //     nsfwLevel: {
-            //         notIn: [NSFWLevel.X, NSFWLevel.Mature] // Enum values here
-            //     },
-            // },
-            orderBy: {
-                likeCount: 'desc', // Order by likeCount
-            },
-            where: {
-                createdAt: {
-                    gte: twoWeeksAgo // Only videos created in the last week
-                }
-            },
-
-            select: {
-                videoId: true,
-                remoteId: true,
-                likeCount: true,
-                heartCount: true,
-                commentCount: true,
-                username: true,
-                meta: true,
-                user: {
-                    select: {
-                        imageUrl: true,
-                    },
+    const randomOffset = Math.floor(Math.random() * totalItems);
+    return await prisma.Image.findMany({
+        where: {
+            category: categoryId,
+            likeCount: {
+                gt: 15 // Only select items with more than 10 likes
+            }
+        },
+        take: 1,
+        skip: randomOffset,
+        select: {
+            category: true,
+            videoId: true,
+            remoteId: true,
+            likeCount: true,
+            heartCount: true,
+            commentCount: true,
+            username: true,
+            meta: true,
+            category: true,
+            user: {
+                select: {
+                    imageUrl: true,
                 },
             },
-        });
+        },
+    });
+}
 
-        return images;
+
+
+// Function to fetch random items from each category
+// async function getRandomItemsFromAllCategories() {
+//     const categories = [0, 1, 2, 3, 4, 5, 6]; // Integer categories
+//     return await Promise.all(categories.map(categoryId =>
+//         getRandomItemFromCategory(categoryId)
+//     ));
+// }
+
+function createWeightedDistribution(categories, weights) {
+    let distribution = [];
+    for (let i = 0; i < categories.length; i++) {
+        for (let j = 0; j < weights[i]; j++) {
+            distribution.push(categories[i]);
+        }
     }
+    return distribution;
+}
 
-    if (method == 'get') {
-        const results = await getTopImages();
+function selectRandomItemsFromArray(array, numberOfItems) {
+    let selectedItems = [];
+    for (let i = 0; i < numberOfItems; i++) {
+        let randomIndex = Math.floor(Math.random() * array.length);
+        selectedItems.push(array[randomIndex]);
+    }
+    console.log(selectedItems)
+    return selectedItems;
+}
 
-        // Map each result to include video URL, thumbnail URL, and DB data
-        const videosWithThumbs: IVideoData[] = results.map(item => ({
-            data: {
-                dbData: item,
-                storage: {
-                    videoUrl: `${VIDEO_BASE_URL}${item.videoId}.mp4`,
-                    thumbUrl: `${THUMBS_BASE_URL}${item.videoId}.jpg`
-                }
+async function getRandomItemsFromAllCategories(pageSize: number) {
+    const categories = [0, 1, 2, 3, 4, 5, 6];
+    const weights =    [1, 1, 1, 3, 2, 1, 1]; // Higher weight for category 3
+
+    const weightedDistribution = createWeightedDistribution(categories, weights);
+    const selectedCategories = selectRandomItemsFromArray(weightedDistribution,pageSize); // Select 5 items, allowing duplicates
+
+    return await Promise.all(selectedCategories.map(categoryId => 
+        getRandomItemFromCategory(categoryId)
+    ));
+}
+
+
+
+// Formatting function
+function formatVideoData(videos) {
+    return videos.map(item => ({
+        data: {
+            dbData: item[0], // Since the result is an array with one item
+            storage: {
+                videoUrl: `${VIDEO_BASE_URL}${item[0].videoId}.mp4`,
+                thumbUrl: `${THUMBS_BASE_URL}${item[0].videoId}.jpg`
             }
-        }));
+        }
+    }));
+}
 
-        // console.log(`Constructed URLs for ${videosWithThumbs.length} videos with thumbnails`);
-        // console.log(videosWithThumbs)
+// Next.js API handler
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+    const { method } = req.query;
+    const pageSize = 5;
 
-        return res.status(200).json(videosWithThumbs);
+    if (method === 'get') {
+        console.log('called')
+        try {
+            const randomItemsResults = await getRandomItemsFromAllCategories(pageSize);
+            const formattedRandomVideos = formatVideoData(randomItemsResults);
+
+            return res.status(200).json(formattedRandomVideos);
+        } catch (error) {
+            console.error('Error:', error);
+            return res.status(500).json({ message: 'Internal Server Error' });
+        }
+    } else {
+        return res.status(405).json({ message: 'Method Not Allowed' });
     }
-
 }
 
 export default handler;
