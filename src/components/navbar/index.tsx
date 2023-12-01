50676207173b3890ff89715511e945ad74009f53
Shelby Thomas <shelbyt@ucsd.edu>
2023-12-01 15:21:57 -0800

    info page

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { useRouter } from 'next/router';
-import { House, User, PaintBrushHousehold } from "@phosphor-icons/react";
-import { useUser } from '@auth0/nextjs-auth0/client';
+import { House, User, Info } from "@phosphor-icons/react";
 import useUserUUID from '../../hooks/useUserUUID';
 import { usePopup } from '../../state/PopupContext';
+import { handleNavigationAway } from '../../state/localStorageHelpers';
 
 
 const Navbar: React.FC = () => {
     const router = useRouter();
-    const { user, isLoading, error } = useUser();
-    const { userState, fetchUserData } = useUserUUID();
+    const { userState } = useUserUUID();
     const { handleLogin } = usePopup();
 
     const navigateToHome = () => {
@@ -22,12 +21,14 @@ const Navbar: React.FC = () => {
             handleLogin(); // Use handleLogin from PopupContext
         }
         if(userState?.isAuthenticated) {
+            handleNavigationAway();
             router.push('/profile');
         }
     };
 
-    const handleLogout = () => {
-        router.push('/api/auth/logout');
+    const handleInfo = () => {
+        // router.push('/api/auth/logout');
+        router.push('/info')
     }
 return (
     <div className="h-16 flex justify-between items-start bg-black px-16" style={{ borderTop: '0.5px solid rgba(255, 255, 255, 0.1)' }}>
@@ -36,7 +37,7 @@ return (
             <p className="text-xs mt-1">Home</p>
         </div>
         <div className="flex flex-col items-center text-white mt-2">
-            <PaintBrushHousehold size={28} color="#fff" weight="fill" onClick={handleLogout} />
+            <Info size={28} color="#fff" weight="fill" onClick={handleInfo} />
         </div>
         <div className="flex flex-col items-center text-white mt-2" onClick={navigateToProfile}>
             {router.pathname === '/profile' ? <User size={28} weight='fill' color="#fff" /> : <User size={32} color="#fff" />}
---
+++ src/pages/info/index.tsx
@@ -0,0 +1,64 @@
+// pages/info.js
+import Head from 'next/head';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+export default function Info() {
+	const router = useRouter();
+	return (
+		<div className="bg-gray-50">
+			<Head>
+				<title>Application Info</title>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+
+			<div className="container mx-auto px-6 py-12">
+				<h1 className="text-4xl md:text-6xl font-bold text-center text-gray-800">
+					About <span className="text-blue-600">Diffusion TV</span>
+				</h1>
+
+				<div className="mt-8 text-gray-600 text-md max-w-2xl mx-auto text-center">
+					<p>
+						Hello! Diffusion TV is Tiktok for AI generated videos. It is a mobile first PWA application build with NextJS/Tailwind/Prisma inspired by short video platforms. This project was born out of a desire to stay on the cutting edge of AI video technology and to shine a light on the amazing work being done by many talented individuals. The best part is, it's open source so everyone can learn how to build this themselves.
+					</p>
+					<br />
+					<p>
+						I am running this project purely out pocket. If you want to support the project give a ⭐ on GitHub and leave issues and feedback. More information can be found there. Thank you!
+					</p>
+
+					<br />
+					<p>
+						Disclaimer: The content in DiffusionTV based 100% on Civitai's API. None of the content is scraped from their website in order to remain compliant to their Terms of Service. All content belongs to the respective owners.
+
+
+					</p>
+					{/* Add more paragraphs as needed */}
+				</div>
+
+				<div className="mt-12 flex justify-center">
+					<Link href="https://github.com/shelbyt/diffusiontv">
+						<a target="_blank" rel="noopener noreferrer" className="btn btn-primary">
+							Support on GitHub
+						</a>
+					</Link>
+				</div>
+
+				<div className="mt-4 flex justify-center">
+					<button
+						onClick={() => router.push('/api/auth/logout')}
+						className="btn btn-outline"
+					>
+						Logout
+					</button>
+
+				</div>
+			</div>
+
+			<footer className="border-t bg-white py-8">
+				<div className="container mx-auto px-6 text-center text-gray-600">
+					&copy; {new Date().getFullYear()} DiffusionTV. All rights reserved.
+				</div>
+			</footer>
+		</div>
+	);
+}
