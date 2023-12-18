ae248f7b078d4c5189adf9b889465a8c175cd5ac
Shelby Thomas <shelbyt@ucsd.edu>
2023-12-18 14:20:24 -0800

    Drop blocking on desktop or non-standalone

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,53 +19,56 @@ const IndexPage: React.FC = () => {
 
     if (isClient) {
         if (isBrowser) {
-            mixpanel.track('Onboard: Browser View')
-            return (
-                <div className="flex flex-col items-center justify-center h-screen bg-black">
-                    <Image
-                        src="/qr.png"
-                        alt="QR Code"
-                        width={512}
-                        height={512}
-                        layout="fixed"
-                    />
-                    <p className="text-white mt-4 text-3xl">Mobile Exclusive 📱</p>
-                </div>
-            );
+
+            mixpanel.track('Visiting From: Browser View')
+            return <Home />;
+            // return (
+            //     <div className="flex flex-col items-center justify-center h-screen bg-black">
+            //         <Image
+            //             src="/qr.png"
+            //             alt="QR Code"
+            //             width={512}
+            //             height={512}
+            //             layout="fixed"
+            //         />
+            //         <p className="text-white mt-4 text-3xl">Mobile Exclusive 📱</p>
+            //     </div>
+            // );
         }
         else if (!isStandalone) {
-            mixpanel.track('Onboard: Mobile Not Standalone')
-            return (
-                <div className="flex h-screen bg-gray-100">
-                    <div className="m-auto text-center">
-                        <Image
-                            src="/logohq.png"
-                            alt="Diffusion TV Logo"
-                            width={144}
-                            height={50}
-                            layout="fixed"
-                        />
-                        <h1 className="text-xl font-bold mb-4">Install Web App</h1>
-                        <p className="mb-8">
-                            Please open with Safari (iOS) or Chrome (Andriod) and add to home screen for a native experience.
-                        </p>
-                        <div className="inline-block p-4 bg-gray-200 rounded-lg">
-                            <span className="align-middle">iOS: Tap </span>
-                            <Export className="inline-block align-middle text-lg" color="#007aff" />
-                            <span className="align-middle"> and click &apos;Add to Home Screen&apos;</span>
-                            <br />
-                            <br />
+            mixpanel.track('Visiting From: Mobile Not Standalone')
+            return <Home />;
+            // return (
+            //     <div className="flex h-screen bg-gray-100">
+            //         <div className="m-auto text-center">
+            //             <Image
+            //                 src="/logohq.png"
+            //                 alt="Diffusion TV Logo"
+            //                 width={144}
+            //                 height={50}
+            //                 layout="fixed"
+            //             />
+            //             <h1 className="text-xl font-bold mb-4">Install Web App</h1>
+            //             <p className="mb-8">
+            //                 Please open with Safari (iOS) or Chrome (Andriod) and add to home screen for a native experience.
+            //             </p>
+            //             <div className="inline-block p-4 bg-gray-200 rounded-lg">
+            //                 <span className="align-middle">iOS: Tap </span>
+            //                 <Export className="inline-block align-middle text-lg" color="#007aff" />
+            //                 <span className="align-middle"> and click &apos;Add to Home Screen&apos;</span>
+            //                 <br />
+            //                 <br />
 
-                            <span className="align-middle">Andriod: Tap </span>
-                            <DotsThreeVertical className="inline-block align-middle text-lg" color="#007aff" />
-                            <span className="align-middle"> and click &apos;Add to Home Screen&apos;</span>
-                        </div>
-                    </div>
-                </div>
-            );
+            //                 <span className="align-middle">Andriod: Tap </span>
+            //                 <DotsThreeVertical className="inline-block align-middle text-lg" color="#007aff" />
+            //                 <span className="align-middle"> and click &apos;Add to Home Screen&apos;</span>
+            //             </div>
+            //         </div>
+            //     </div>
+            // );
         }
         else {
-            mixpanel.track('Onboard: Mobile Standalone')
+            mixpanel.track('Visiting From: Mobile Standalone')
             return <Home />;
         }
     }
